c55c1b659f7d06eec10626b7d08c81f579606ff6
ziyue-guan <guanziyue@gmail.com>
2018-04-29 12:34:45 +1000

    add comment for sentiment analysis
    indicate that the output of function

--- tweet/Analysis.py
+++ tweet/Analysis.py
@@ -1,5 +1,6 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-# analyze sentiment. input is string,
+# analyze sentiment. input is string,return a dictionary including pos: postive probability.
+#neg: negative probability. compoud: when positive int, positive, negtive int ,negative. zero, neutual
 def sentiment_analysis(content):
     senti_analyzer=SentimentIntensityAnalyzer()
     return senti_analyzer.polarity_scores(content)
