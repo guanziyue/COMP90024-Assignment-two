4beab68c58ed93af1594c803d4f380a38f1652cc
guanziyue <30882822+guanziyue@users.noreply.github.com>
2018-05-10 12:56:57 +1000

    Add files via upload

---
+++ ansible/roles/web/files/web/resources/js/Mood.js
@@ -0,0 +1,35 @@
+mood_callback(
+    {"type":"FeatureCollection","features":[
+        {"type":"Feature",
+            "geometry":{"type":"Point","coordinates":[151.215256, -33.856159]},
+            "id":"Sydney_New_South_Wales",
+            "malecount":7299,
+            "malerate":6.785,
+            "femalecount":8420,
+            "femalerate": 8.793
+        },
+        {"type":"Feature",
+            "geometry":{"type":"Point","coordinates":[144.946014,-37.815018]},
+            "id":"Melbourne_Victoria",
+            "malecount":3033,
+            "malerate":6.425,
+            "femalecount":3848,
+            "femalerate": 7.668
+        },
+        {"type":"Feature",
+            "geometry":{"type":"Point","coordinates":[115.8614, -31.95224]},
+            "id":"Perth_Western_Australia",
+            "malecount":642,
+            "malerate":6.506,
+            "femalecount":601,
+            "femalerate": 7.806
+        },
+        {"type":"Feature",
+            "geometry":{"type":"Point","coordinates":[153.021072,-27.470125]},
+            "id":"Brisbane_Queensland",
+            "malecount":30334,
+            "malerate":5.723,
+            "femalecount":42875,
+            "femalerate": 7.744
+        }]
+});
